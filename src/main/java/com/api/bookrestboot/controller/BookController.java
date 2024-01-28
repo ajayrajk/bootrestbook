60a0c319d28527ff60424d6cb3ccde28c1083cbc
Ajay RajPut <ajayhbti11@gmail.com>
2024-01-28 13:00:22 +0530

    Handle https status

--- src/main/java/com/api/bookrestboot/controller/BookController.java
+++ src/main/java/com/api/bookrestboot/controller/BookController.java
@@ -1,8 +1,12 @@
 package com.api.bookrestboot.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,38 +34,74 @@ public class BookController {
 	
 	//Get all book handler
 	@GetMapping("/books")
-	public List<Book> getBooks(){
-		return this.bookService.getAllBooks();
+	public ResponseEntity<List<Book>> getBooks(){
+		List<Book> bList= this.bookService.getAllBooks();
+		if(bList.size()<=0) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+		return ResponseEntity.of(Optional.of(bList));
 	}
 	
 	
 	//Get single book handler
 	@GetMapping("/books/{id}")
-	public Book getBookById(@PathVariable("id") int id) {
-		return bookService.getBookById(id);
+	public ResponseEntity<Book> getBookById(@PathVariable("id") int id) {
+		Book book= bookService.getBookById(id);
+		try {
+			if(book==null)
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+				
+				return ResponseEntity.of(Optional.of(book));
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+		
 	}
 	
 	
 	//new book handler
 	@PostMapping("/books")
-	public Book addBook(@RequestBody Book b) {
-		
-		bookService.addBook(b);
-		
-		return b;
+	public ResponseEntity<Book> addBook(@RequestBody Book b) {
+		Book book=null;
+		try {
+			book=bookService.addBook(b);
+			System.out.println(book);
+			
+          return ResponseEntity.of(Optional.of(b));
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.
+					status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 	
 	// delete book handler
 	@DeleteMapping("/books/{bookid}")
-	public void deleteBookById(@PathVariable("bookid") int id) {
-		bookService.deleteBookById(id);
+	public ResponseEntity<Void> deleteBookById(@PathVariable("bookid") int id) {
+		try {
+			bookService.deleteBookById(id);
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.build();
+		}
 	}
 	
 	//update book handler 
 	@PutMapping("/books/{bookid}")
-	public Book updateBookById(@RequestBody Book book, @PathVariable("bookid") int bid) {
+	public ResponseEntity<Book> updateBookById(@RequestBody Book book, @PathVariable("bookid") int bid) {
+		try {
 		bookService.updateBook(book,bid);
-		return book;
+		return ResponseEntity.of(Optional.of(book));
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.
+					status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 	
 	
--- src/main/java/com/api/bookrestboot/entities/Book.java
+++ src/main/java/com/api/bookrestboot/entities/Book.java
@@ -1,9 +1,16 @@
 package com.api.bookrestboot.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "books")
 public class Book {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
 	private String title;
 	private String author;
